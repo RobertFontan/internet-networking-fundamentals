d2bf04d8cde139a5018a5537979e1f4f06ac8089
Robert Fontan <robertfontan@gmail.com>
2023-02-23 21:39:53 -0500

    Basic Architecture done

--- socket programming/client.java
+++ socket programming/client.java
@@ -4,7 +4,35 @@ import java.util.Scanner;
 
 public class client {
     public static void main(String[] args) throws IOException{
+        Socket clientSocket;
+        PrintWriter outputToServer;
+        BufferedReader inputFromServer;
+        int port = 5414;
 
+        clientSocket = new Socket("localhost", port);
+        outputToServer = new PrintWriter(clientSocket.getOutputStream(), true);
+        inputFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        Scanner sc = new Scanner(System.in); // to write in the terminal
+        // each line is scanned and sent to server
+        String toServer = "";
+        while(!"bye".equalsIgnoreCase(toServer)){
+            toServer = sc.nextLine();
+            //System.out.println("Send to server: " + toServer); // this could be del
+            outputToServer.println(toServer);
+            String responseFromServer = inputFromServer.readLine();
+
+            System.out.println(responseFromServer); // here lies the reponse
+        
+        
+        }
+        inputFromServer.close();
+        outputToServer.close();
+        clientSocket.close();
+        sc.close();
+    }
+}
+
+        /*
         Socket s = null;
         InputStreamReader inputStreamReader = null;
         OutputStreamWriter outputStreamWriter = null;
@@ -59,3 +87,4 @@ public class client {
 
     }
 }
+*/
--- socket programming/server.java
+++ socket programming/server.java
@@ -3,6 +3,114 @@ import java.io.*;
 
 public class server {
     public static void main(String[] args) throws IOException{
+        // creating joke files
+        try{
+            FileWriter joke1 = new FileWriter("joke1.txt");
+            joke1.write("What do they call a group of network engineers? An outage. ");
+            joke1.close();
+            FileWriter joke2 = new FileWriter("joke2.txt");
+            joke2.write("I would tell you a joke about UDP, but you probably wouldn't get it.");
+            joke2.close();
+            FileWriter joke3 = new FileWriter("joke3.txt");
+            joke3.write("DNS is the root of all problems.");
+            joke3.close();
+
+          //  JOKE_FILES.toFile(joke1);
+        }
+        catch(IOException e){
+            System.out.println("An error occured creating the jokes");
+            e.printStackTrace();
+        }
+
+
+        ServerSocket serverSocket;
+        int port = 5414;
+        Socket clientSocket;
+        PrintWriter outputToClient;
+        BufferedReader inputFromClient;
+
+        System.out.println("Start Socket Server on Port: " + port);
+        serverSocket = new ServerSocket(port); 
+        clientSocket = serverSocket.accept();
+        System.out.println("Connection: " + serverSocket.getInetAddress());
+        
+        //i/o from client
+        outputToClient = new PrintWriter(clientSocket.getOutputStream(), true);
+        inputFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String message = "";
+        
+        // communication will continue forever until bye
+        outputToClient.println("Hello!");
+
+        while(!"bye".equalsIgnoreCase(message)){
+            message = inputFromClient.readLine(); //reading one line at a time from client
+            System.out.println("Client: " + message);
+
+            if(message.contains("bye"))
+                break;
+            if(message == null)
+                continue;
+            if(message.contains("Joke")){
+                // reading the joke number
+                try{
+                    int jokeNumber = Integer.parseInt(message.substring(5).trim());
+                    
+                    //String jokeFilename = JOKE_FILES[jokeNumber - 1];
+                    String jokeFilename = "";
+
+                    switch(jokeNumber){
+                        case 1:
+                            jokeFilename = "joke1.txt";
+                            break;
+                        case 2:
+                            jokeFilename = "joke2.txt";
+                            break;
+                        case 3:
+                            jokeFilename = "joke3.txt";
+                            break;
+                        default:
+                            outputToClient.println("Try numbers 1-3");
+                    }
+                    
+                    File jokeFile = new File(jokeFilename);
+                
+                    
+                    BufferedReader jokeReader = new BufferedReader(new FileReader(jokeFile));
+                    String line;
+                    while ((line = jokeReader.readLine()) != null) {
+                        outputToClient.println(line);
+                    }
+                    jokeReader.close();
+                }
+                catch (Exception e){
+                    outputToClient.println("Try Joke <number>");
+                }
+                
+            }
+            else {
+                outputToClient.println("Invalid command :<");
+            }
+
+        }
+        outputToClient.println("Bye!");
+        // breaking from while closes everything
+        inputFromClient.close();
+        outputToClient.close();
+        clientSocket.close();
+        serverSocket.close();
+        System.out.println("Server Socket closed");
+    
+    }
+}
+
+
+
+
+
+
+
+
+        /*
         InputStreamReader inputStreamReader = null;
         OutputStreamWriter outputStreamWriter = null;
         BufferedReader bufferedReader = null;;
@@ -62,3 +170,4 @@ public class server {
     }
 }
 
+*/
