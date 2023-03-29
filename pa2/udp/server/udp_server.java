87868c633629c538d9314ba7e5cbdc69fb2e9b86
Robert Fontan <robertfontan@gmail.com>
2023-03-29 01:27:30 -0400

    UDP sending 10 images done

--- pa2/udp/server/udp_server.java
+++ pa2/udp/server/udp_server.java
@@ -1,58 +1,66 @@
 package udp.server;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
 
-import javax.imageio.ImageIO;
+import java.io.*;
+import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-/*
- * /* -- Removes all received files
- * rm *received* 
- *  
- */
 public class udp_server {
     public static void main(String[] args) throws IOException {
-        DatagramSocket datagramSocket = new DatagramSocket(5414);
+        DatagramSocket socket = new DatagramSocket(8888);
+        byte[] buf = new byte[256];
 
-        System.out.println("Server listening on port 5414...");
-        
-        while (true) {
-            byte[] buffer = new byte[1024];
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-            datagramSocket.receive(packet);
+        System.out.println("Waiting for client connection...");
 
-            InetAddress clientAddress = packet.getAddress();
-            int clientPort = packet.getPort();
-            System.out.println("Connection from client " + clientAddress + " on port " + clientPort);
-            
-            for(int i = 1; i <= 10; i++){
-
-                BufferedImage img = ImageIO.read(new File("./images/sample" + i + ".png"));
-                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-
-                ImageIO.write(img, "png", byteArrayOutputStream);
-
-                byte[] imageData = byteArrayOutputStream.toByteArray();
-                int packetSize = 1024; // Maximum packet size
-                int packetCount = (int) Math.ceil((double) imageData.length / packetSize);
-                
-                for (int j = 0; j < packetCount; j++) {
-                    int offset = j * packetSize;
-                    int length = Math.min(packetSize, imageData.length - offset);
-                    byte[] data = new byte[length];
-                    System.arraycopy(imageData, offset, data, 0, length);
-                    DatagramPacket sendPacket = new DatagramPacket(data, length, clientAddress, clientPort);
-                    datagramSocket.send(sendPacket);
-                }
-                
-                System.out.println("Image "+ i + " sent to client " + clientAddress + " on port " + clientPort);
+        // Wait for connection message from the client
+        DatagramPacket connectionPacket = new DatagramPacket(buf, buf.length);
+        socket.receive(connectionPacket);
+        String received = new String(connectionPacket.getData(), 0, connectionPacket.getLength());
+        if (!"CONNECT".equals(received)) {
+            System.out.println("Invalid connection message. Exiting.");
+            socket.close();
+            return;
         }
 
+        System.out.println("Client connected. Sending images...");
 
+        InetAddress clientAddress = connectionPacket.getAddress();
+        int clientPort = connectionPacket.getPort();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Attempting image " + i);
+
+            String fileName = "images/sample" + i + ".png";
+            byte[] imageData = Files.readAllBytes(Paths.get(fileName));
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            DataOutputStream dos = new DataOutputStream(baos);
+            dos.writeInt(imageData.length);
+            dos.write(imageData);
+            dos.flush();
+            byte[] dataToSend = baos.toByteArray();
+
+            int offset = 0;
+            int packetSize = 1024;
+            // dataToSend.length = 170148
+            while (offset < dataToSend.length) {
+                int bytesToSend = Math.min(packetSize, dataToSend.length - offset);
+                byte[] buffer = new byte[bytesToSend];
+                //System.out.println(offset);
+                System.arraycopy(dataToSend, offset, buffer, 0, bytesToSend);
+                DatagramPacket packet = new DatagramPacket(buffer, bytesToSend, clientAddress, clientPort);
+                socket.send(packet);
+                offset += bytesToSend;
+
+                
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Image " + i + " sent");
         }
+        socket.close();
     }
 }
--- pa2/udp/udp_client.java
+++ pa2/udp/udp_client.java
@@ -1,49 +1,61 @@
 package udp;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.*;
-
-import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
+import java.nio.ByteBuffer;
 
 public class udp_client {
     public static void main(String[] args) throws IOException {
-        DatagramSocket socket = new DatagramSocket();
-        byte[] buffer = new byte[1024];
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-        
-        socket.send(new DatagramPacket(new byte[] { 0 }, 1, InetAddress.getLocalHost(), 5414));
-        System.out.println("Requesting image from server...");
-        
-        //for
-        for(int i = 1; i <= 10; i++){
-            socket.receive(packet);
-            
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            while (true) {
-                outputStream.write(packet.getData(), 0, packet.getLength());
-                if (packet.getLength() < buffer.length) {
-                    break; // End of image marker received
-                }
-                packet = new DatagramPacket(buffer, buffer.length);
+        DatagramSocket socket = new DatagramSocket(9999);
+
+        // Send connection message to the server
+        byte[] buf = "CONNECT".getBytes();
+        InetAddress serverAddress = InetAddress.getByName("localhost");
+        DatagramPacket connectionPacket = new DatagramPacket(buf, buf.length, serverAddress, 8888);
+        socket.send(connectionPacket);
+
+        for (int i = 1; i <= 10; i++) {
+            // ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            // byte[] buffer = new byte[1024];
+            // int totalBytesRead = 0;
+            // int bytesRead;
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int expectedSize = -1;
+            int totalBytesRead = 0;
+
+
+
+            while (true) 
+            {
+                byte[] buffer = new byte[1024];
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
                 socket.receive(packet);
+
+                int bytesRead = packet.getLength();
+                baos.write(buffer, 0, bytesRead);
+                totalBytesRead += bytesRead;
+
+                if (expectedSize == -1 && totalBytesRead >= 4) {
+                    expectedSize = ByteBuffer.wrap(baos.toByteArray(), 0, 4).getInt();
+                }
+
+                if (expectedSize != -1 && totalBytesRead >= expectedSize + 4) {
+                    byte[] imageData = new byte[expectedSize];
+                    System.arraycopy(baos.toByteArray(), 4, imageData, 0, expectedSize);
+
+                    ByteArrayInputStream imageBais = new ByteArrayInputStream(imageData);
+                    BufferedImage image = ImageIO.read(imageBais);
+                    ImageIO.write(image, "png", new File("received_image" + i + ".png"));
+
+                    break;
+                }
             }
-            
-            byte[] imageBuffer = outputStream.toByteArray();
-            BufferedImage image = ImageIO.read(new ByteArrayInputStream(imageBuffer));
-            if (image == null) {
-                System.err.println("Error decoding image.");
-                return;
-            }
-            
-            File outputFile = new File("./received_image" + i + ".png");
-            try {
-                ImageIO.write(image, "png", outputFile);
-            } catch (IOException e) {
-                System.err.println("Error writing image " + i + " to file: " + e.getMessage());
-                return;
-            }
-            
-            System.out.println("Image " + i + " received from server and saved to file " + outputFile.getPath() + ".");
+
+            System.out.println("Image " + i + " recieved");
+
         }
         socket.close();
     }
